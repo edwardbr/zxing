8760a2000a239807d032ac70d73d88752c018377
Edward Boggis-Rolfe <edward@boggis-rolfe.com>
2019-09-02 13:27:34 +0100

    added bigint as a dependency

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -5,3 +5,14 @@ hunter_config(Qt VERSION "5.12.3"
         ANDROID_PLATFORM=${ANDROID_PLATFORM}
         CMAKE_ANDROID_ARCH=${CMAKE_ANDROID_ARCH}
     )
+    
+hunter_config(bigint
+    URL https://github.com/edwardbr/bigint/archive/master.tar.gz
+    SHA1 7b7af0a373c42bc3a0537e7af673892af31bec12
+    CMAKE_ARGS
+        ANDROID_STL=${ANDROID_STL}
+        ANDROID_ABI=${ANDROID_ABI}
+        ANDROID_PLATFORM=${ANDROID_PLATFORM}
+        CMAKE_ANDROID_ARCH=${CMAKE_ANDROID_ARCH}
+        QT_CMAKE=${QT_CMAKE}
+        )
